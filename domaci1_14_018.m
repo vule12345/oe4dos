857a61e17295a4a2004ccf6d8acb1c07e5c7e62b
Vuk <vuk.d.vukomanovic@gmail.com>
2017-11-06 19:34:38 +0100

    Added second part of first homework

--- domaci1_14_018.m
+++ domaci1_14_018.m
@@ -1,5 +1,6 @@
 % OE4DOS First homework 11/12/2017
 % Student : Vuk Vukomanovic 2014/0018
+% Git: 
 % This script contains first 4 tasks
 clear
 clc
@@ -12,12 +13,13 @@ warning('off', 'images:initSize:adjustingMag')
 % In first part of code methods are used in RGB picture, in second half all
 % methods are used in LAB picture and than convereted back to RGB
 
+disp('First part of homework starts here.');
 % Input image
-disp('Reading image')
+disp('Reading ,street.tif,, image')
 street = imread('ulazne_slike/street.tif');
 figure
 imshow(street);
-set(gcf, 'Name', 'Ulazna slika');
+set(gcf, 'Name', 'Ulazna slika, street');
 title('Ulazna slika','Interpreter','LaTex','FontSize',16),grid on
 
 % Greyscale 
@@ -27,13 +29,15 @@ street_gray = rgb2gray(street);
 street_hist = imhist(street_gray);
 figure;
 bar(street_hist/numel(street_gray));
-set(gcf, 'Name', 'Histogram ulazne slike');
+set(gcf, 'Name', 'Histogram ulazne slike, street');
 title('Histogram ulazne slike','Interpreter','LaTex','FontSize',16),grid on
+xlabel('Vrednost piskela','Interpreter','LaTex','FontSize',16)
 
 %Scaling image
 street_scaled = mat2gray(street);
 %%% 1.a : RGB image
 disp('RGB image processing');
+
 % Logarithmic contrast enhancement
 k=100;
 street_log = im2uint8(log(1+k*street_scaled)/log(k));
@@ -56,11 +60,11 @@ figure
 imshow(street_eq_hist)
 set(gcf, 'Name', 'Slika nakon upotrebe ekvalizacije histograma');
 title('Slika nakon upotrebe ekvalizacije histograma','Interpreter','LaTex','FontSize',16),grid on
-
 figure
 equ_histogram = imhist(street_eq_hist);
 bar(equ_histogram/numel(street_eq_hist))
 title('Histogram nakon ekvalizacije','Interpreter','LaTex','FontSize',16),grid on
+xlabel('Vrednost piskela','Interpreter','LaTex','FontSize',16)
 
 %%% 1.b : LAB image
 disp('Lab image processing, this may take a while...')
@@ -78,7 +82,7 @@ imshow(street_lab_log);
 set(gcf, 'Name', 'Slika nakon upotrebe log-a na Lab slici');
 title('Slika nakon upotrebe log-a na Lab slici','Interpreter','LaTex','FontSize',16)
 
-% Logarithmic contrast enhancement
+% Gamma contrast enhancement
 street_lab_gamma = street_lab;
 street_lab_gamma (:,:,1) = (L.^0.35)*max_luminosity;
 street_lab_gamma  = lab2rgb(street_lab_gamma ,'ColorSpace','adobe-rgb-1998');
@@ -95,7 +99,93 @@ figure
 imshow(street_lab_eq);
 set(gcf, 'Name', 'Slika nakon upotrebe gamma metode na Lab slici');
 title('Slika nakon upotrebe gamma metode na Lab slici','Interpreter','LaTex','FontSize',16)
-disp('First part of homework end here.')
+disp('First part of homework ends here.')
 
+% Write to fail and calculate variance
+std_deviation = sqrt(var(double(street_log(:))));
+disp("Standardn deviation of contrast enhanced image :")
+disp(std_deviation)
+
+imwrite(street_log,'izlazne_slike/street_out.jpg', 'Quality',100);
+
+% Histogram of output image
+bar(imhist(street_log)/numel(street_log));
+set(gcf, 'Name', 'Histogram izlazne slike');
+title('Histogram izlazne slike','Interpreter','LaTex','FontSize',16)
+xlabel('Vrednost piskela','Interpreter','LaTex','FontSize',16)
+
+disp('First part of homework ends here.')
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%
+%% 2. Sharpen image ,,lange.jpg,, with various methods to find best one
+% Report holds explanations of why certain method were used
+
+disp('Second part of homework starts here.')
+
+% Input image
+disp('Reading image')
+lange = double(imread('ulazne_slike/lange.jpg'));
+disp('Processing')
+figure
+imshow(lange/255);
+set(gcf, 'Name', 'Ulazna slika, lange');
+title('Ulazna slika, lange','Interpreter','LaTex','FontSize',16)
+
+% Low pass filter method
+low_pass_mask = fspecial('average',5);
+lange_low_freq = imfilter(lange, low_pass_mask, 'replicate');
+lange_high_pass = lange - lange_low_freq;
+k=1;
+lange_filtered = uint8(lange + k*lange_high_pass);
+figure
+imshow(lange_filtered);
+set(gcf, 'Name', 'Low pass filter metoda');
+title('Low pass filter metoda','Interpreter','LaTex','FontSize',16)
+
+
+% Laplacian filter methods
+laplacian_mask =  [0 1 0; 1 -4 1; 0 1 0];
+lange_laplacian_edge = imfilter(lange, laplacian_mask, 'replicate');
+lange_laplacian = uint8(lange + lange_laplacian_edge);
+figure
+imshow(lange_laplacian);
+set(gcf, 'Name', 'Laplacian filter');
+title('Laplacian filter','Interpreter','LaTex','FontSize',16)
+
+
+% Sobel-gradient method
+
+% Calculting gradient
+sobel = [-1 0 1; -2 0 2; -1 0 1];
+lange_sobel =  imfilter(lange, sobel, 'replicate');
+figure
+imshow(lange_sobel, []);
+set(gcf, 'Name', 'Sobel gradient ulazne slike');
+title('Sobel gradient ulazne slike','Interpreter','LaTex','FontSize',16)
+
+% Smoothing gradient response
+low_pass_mask_1 = fspecial('average',10);
+lange_sobel_low = imfilter(lange_sobel, low_pass_mask_1, 'replicate');
+
+figure
+imshow(lange_sobel_low,[]);
+set(gcf, 'Name', 'Usrednjen gradient');
+title('Usrednjen gradient','Interpreter','LaTex','FontSize',16)
+
+temp_lange = mat2gray(lange_sobel_low)*255;
+
+% Adding image with images to original image
+lange_sobel_result = im2uint8(mat2gray(lange+temp_lange));
+figure
+imshow(lange_sobel_result);
+set(gcf, 'Name', 'Slika nakon sabiranja pocente slike i dobijeng rezultata');
+title('Slika nakon sabiranja','Interpreter','LaTex','FontSize',16)
+
+lange_sobel_result = imadjust(lange_sobel_result);
+figure
+imshow(lange_sobel_result)
+set(gcf, 'Name', 'Slika nakon porpravke kontrasta');
+title('Slika nakon porpravke kontrasta','Interpreter','LaTex','FontSize',16)
+
+imwrite(lange_sobel_result,'izlazne_slike/lange_sharp.jpg', 'Quality',100);
+
+disp('Second part of homework ends here.')
---
+++ sharpening_combined.m
@@ -0,0 +1,61 @@
+%% Skripta koja kombinuje metode sa Laplasijanom i gradijentom. Ova metoda
+% je opisana u knjizi Digital image processing (Gonazles). Ideja je da se
+% iskoriste prednosti obe metode. Tj. odziv laplasijana ce biti pun suma,
+% dok odziv metode sa gradijentom ce imati dosta manje, naravno gradijent
+% upravo zbog toga gubi dosta informacija. Ideja je da se kombinovanje ove
+% dve metode napravi kvalitenija slika.
+
+%Note: Ovaj fajl odstupa od komentara na engleskom
+
+close all
+clear 
+clc
+
+% Input image
+lange = double(imread('ulazne_slike/lange.jpg'));
+figure
+imshow(lange/255);
+set(gcf, 'Name', 'Ulazna slika, lange');
+title('Ulazna slika, lange','Interpreter','LaTex','FontSize',16),grid on
+
+a=mat2gray(lange);
+
+% Krosicnje laplaciana za dobijanje pojacnih ivica
+laplacian_mask =  [0 1 0; 1 -4 1; 0 1 0];
+laplacian_res = mat2gray( imfilter(a, laplacian_mask, 'replicate'));
+lange_ench = mat2gray(a + laplacian_res);
+figure
+imshow(laplacian_res,[]);
+set(gcf, 'Name', 'Laplacian filter');
+figure
+imshow(lange_ench,[]);
+set(gcf, 'Name', 'Sabrano');
+
+% Koriscenje sobel gradienta
+sobel = [-1 -2 -1; 0 0 0; 1 2 1];
+gradient =  imfilter(a, sobel, 'replicate');
+figure
+imshow(gradient, []);
+set(gcf, 'Name', 'Sobel gradient');
+
+% Usrednjavanje prehtodnoh rezultata
+low_pass_mask = fspecial('average',20);
+smoothed_gradient = mat2gray(imfilter(gradient,low_pass_mask,'replicate'));
+figure
+imshow(smoothed_gradient,[]);
+set(gcf, 'Name', 'Smoothed Gradient');
+
+%Koriscenje maske
+mask_multiplied = mat2gray(lange_ench.*smoothed_gradient);
+
+figure
+imshow(mask_multiplied,[]);
+set(gcf, 'Name', 'Nakon primene maske');
+
+final= mat2gray(a+mask_multiplied);
+figure
+imshow(final, []);
+set(gcf, 'Name', 'g');
+
+
+
